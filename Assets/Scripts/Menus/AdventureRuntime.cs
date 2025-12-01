17d0790260d9b7825c9765fce54ef413a35349a1
DigitalFresh <digital.freshverlag@gmail.com>
2025-12-02 00:53:22 +0100

    Delete Assets directory

--- Assets/Scripts/Menus/AdventureRuntime.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Простой рантайм-контекст: хранит выбранный AdventureAsset между сценами (через static).
-/// </summary>
-public static class AdventureRuntime
-{
-    public static AdventureAsset SelectedAdventure;
-}
